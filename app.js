fcde635bc1b5f49fde299fb37bc91ade2eb1fd9b
Anton Dementiev <anton.dementyev@gmail.com>
2021-10-04 00:22:33 +0300

    Changed mysql connection

--- app.js
+++ app.js
@@ -3,26 +3,27 @@ const mysql = require('mysql');
 const app = express();
 const port = 3000;
 
-let cn = mysql.createConnection(
-	{
-		host     : 'localhost',
-		user     : 'anton',
-		password : '123',
-		database : 'test'
-	  }
-);
 
-cn.connect();
-
-cn.query("select * from test", function(error, results, fields){
-	if (error) throw error;
-	console.log("Successfully connected to the database on the server");
-});
-
-cn.end();
 
 
 app.get('/', (req, res) => {
+	let cn = mysql.createConnection(
+		{
+			host     : 'localhost',
+			user     : 'anton',
+			password : '123',
+			database : 'test'
+		  }
+	);
+	
+	cn.connect();
+	
+	cn.query("select * from test", function(error, results, fields){
+		if (error) throw error;
+		console.log("Successfully connected to the database on the server");
+	});
+	
+	cn.end();
 	res.send('This sample app was developed for Frontend Masters course. Pushed from Github repo');
 
 });
