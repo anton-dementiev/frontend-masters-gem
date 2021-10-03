93f23622739ca738cf5b5cf0063d7645d0249335
Anton Dementiev <anton.dementyev@gmail.com>
2021-10-04 00:33:24 +0300

    another

---
+++ cn.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+let cn = mysql.createConnection(
+    {
+        host     : 'localhost',
+        user     : 'anton',
+        password : '123',
+        database : 'test'
+      }
+);
+
+cn.connect();
+
+cn.query("select * from test", function(error, results, fields){
+    if (error) throw error;
+    console.log("Successfully connected to the database on the server");
+});
+
+cn.end();
