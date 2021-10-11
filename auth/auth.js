709757ae5da5b92143c9740e757291d5b6e5104a
Anton Dementiev <anton.dementyev@gmail.com>
2021-10-11 18:49:47 +0300

    Fixed auth error

--- auth/auth.js
+++ auth/auth.js
@@ -3,7 +3,7 @@ const crypto = require('crypto');
 const checkSecret = (req, res, next) => {
     
     const temp = "0e8875debb9cbe4f5c2ac1a625b030665ae32e0ac2c4f03827d6d69441a91743";
-    const authHeader = req.headers.authorization.split(' ')[1];
+    const authHeader = req.headers.authorization ? req.headers.authorization.split(' ')[1] : false;
     console.log(authHeader);
     const secret = process.env.API_KEY;
     const message = process.env.MESSAGE;
@@ -11,7 +11,7 @@ const checkSecret = (req, res, next) => {
     const cipher = crypto.createHmac("sha256", secret).update(message).digest('hex');
     console.log(cipher);
 
-    if (cipher !== authHeader) {
+    if (!authHeader | cipher !== authHeader) {
         res.status(403).json({message: "No credentials provided. Fuck off, Oleg!"})
     } else {
         next();
